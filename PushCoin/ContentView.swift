babc6f3daef2df447a9880d8a7c782508bc5eacc
Dmytro Bielorusov <dmytro.bielorusov@gmail.com>
2022-09-02 15:08:40 +0300

    Initial Commit

---
+++ PushCoin/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PushCoin
+//
+//  Created by Dmitriy Belorusov on 9/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PushCoin/PushCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushCoinApp.swift
+//  PushCoin
+//
+//  Created by Dmitriy Belorusov on 9/2/22.
+//
+
+import SwiftUI
+
+@main
+struct PushCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
